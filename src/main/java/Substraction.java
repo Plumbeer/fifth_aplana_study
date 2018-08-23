f443feebb59782276da4e3c2e65bd5fb6741b405
Plumbeer <30840911+Plumbeer@users.noreply.github.com>
2018-08-23 09:21:15 +0300

    Initial commit

---
+++ src/main/java/Substraction.java
@@ -0,0 +1,8 @@
+class Substraction {
+   double subNum;
+
+    void substract(double firstNum, double secondNum) {
+        subNum = firstNum / secondNum;
+        System.out.printf("Частное: %.4f", subNum);
+    }
+}
